ffeda37f837313b5f1b10ca12c8b53715e9f1897
jechlee <jechlee@JECHLEE-KR.kr.oracle.com>
2019-08-21 16:41:58 +0900

    Initial commit2

--- main.tf
+++ main.tf
@@ -2,9 +2,60 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
+variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.key_name}"
+  public_key = "${tls_private_key.example.public_key_openssh}"
+}
+
 resource "aws_instance" "example" {
 //    ami = "ami-0c55b159cbfafe1f0"  
 //    ami ="ami-03170618b41df9458"
-    ami = "ami-0761fc884501dd08f"
+    ami = "ami-095ca789e0549777d"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    key_name = "${aws_key_pair.generated_key.key_name}"
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+
+
