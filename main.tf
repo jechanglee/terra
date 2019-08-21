160ad712a6fc98424bfcb53f05ee513f07b15ab7
jechlee <jechlee@JECHLEE-KR.kr.oracle.com>
2019-08-21 17:08:06 +0900

    Initial commit3

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ resource "aws_key_pair" "generated_key" {
 }
 
 resource "aws_instance" "example" {
-//    ami = "ami-0c55b159cbfafe1f0"  
-//    ami ="ami-03170618b41df9458"
-    ami = "ami-095ca789e0549777d"
+    ami = "ami-0fd02cb7da42ee5e0"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.instance.id]
     key_name = "${aws_key_pair.generated_key.key_name}"
